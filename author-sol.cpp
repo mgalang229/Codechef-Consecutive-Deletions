fe9e39bc7dcdf9e88c1b9f44f35c27367f7af4d2
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-10-21 21:01:09 +0800

    Update author-sol.cpp

--- author-sol.cpp
+++ author-sol.cpp
@@ -14,22 +14,25 @@ int main() {
 		for (int i = 1; i <= n; i++) {
 			cin >> a[i];
 		}
-		// find the no. of 1s at every position (prefix sum)
+		// find the no. of 1's at every position (use prefix sum)
 		vector<int> pref(n + 1, 0);
 		for (int i = 1; i <= n; i++) {
 			pref[i] = pref[i - 1] + a[i];
 		}
-		// find a subarray with length 'k' that has the minimum sum
-		// using this subarray, we can change all the remaining 1s (one-by-one), either
-		// that 1 will be on the leftmost or rightmost position of the subarray
-		int total_ones = pref[n];
-		int min_subarray = k;
+		// find the subarray of length 'k' with the minimum sum
+		// we can see that for every operation, the chosen subarray can be used to transform
+		// all the remaining ones one-by-one (either from current position to the left side or
+		// current position to the right side)
+		long long total_ones = pref[n];
+		long long min_subarray = k;
 		for (int i = k; i <= n; i++) {
-			int cur = pref[i] - pref[i - k];
-			min_subarray = min(min_subarray, cur);
+			long long subarray = pref[i] - pref[i - k];
+			min_subarray = min(min_subarray, subarray);
 		}
-		// to get the answer, calculate the sum of the subarray that we've chosen and all the remaining ones
-		cout << ((min_subarray * (min_subarray + 1)) / 2) + (total_ones - min_subarray) << '\n';
+		// to get the number of operations for the chosen subarray, we need to use the sum formula
+		// and add it to the remaining 1's in the sequence after changing the subarray to all 0's
+		long long res = min_subarray * (min_subarray + 1) / 2 + total_ones - min_subarray;
+		cout << res << '\n';
 	}
 	return 0;
 }
